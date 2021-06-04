cdedb2e71677b33c49b88338ad56eb1de6e9726b
Felipe Milla <felipe.milla@haulmer.com>
2021-06-04 09:47:17 -0400

    inicial laravel

---
+++ memoria/database/migrations/2021_06_04_130414_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('identificador')->unique()->nullable();
+            $table->string('titulo');
+            $table->bigInteger('precio');
+            $table->string('imagen');
+            $table->string('ubicacion');
+            $table->string('link')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
