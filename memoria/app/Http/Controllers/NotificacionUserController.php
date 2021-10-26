8a27e6b4fabcc1e68fe48fcd4cc5533dec819e61
Felipe Milla <felipe.milla@haulmer.com>
2021-10-25 21:53:25 -0300

    avance general 6.5

---
+++ memoria/app/Http/Controllers/NotificacionUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NotificacionUser;
+use Illuminate\Http\Request;
+
+class NotificacionUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NotificacionUser  $notificacionUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NotificacionUser $notificacionUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NotificacionUser  $notificacionUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NotificacionUser $notificacionUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NotificacionUser  $notificacionUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NotificacionUser $notificacionUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NotificacionUser  $notificacionUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NotificacionUser $notificacionUser)
+    {
+        //
+    }
+}
